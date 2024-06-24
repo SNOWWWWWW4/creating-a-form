5c23c00c2577cfb6cad2df5e88c1917de092ae02
SNOWWWWWW4 <lescober7@gmail.com>
2024-06-24 14:09:33 -0700

    small change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Crating a Form",
+  title: "Creating a Form",
   description: "Created with Next.js, Material UI, and Tailwind CSS",
 };
 
