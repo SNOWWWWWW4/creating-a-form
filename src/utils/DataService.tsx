499489ebe7df5709169729121d7e1732de47d81b
sinnchinn <sinathachin@gmail.com>
2024-06-27 14:31:37 -0700

    data services fix

--- src/utils/DataService.tsx
+++ src/utils/DataService.tsx
@@ -1,4 +1,23 @@
-export const getUser = async () => {
-    const promise = await fetch('https://codestackform.azurewebsites.net/AddUser');
-    const data = await promise
+import { IFormProps } from "@/interfaces/interface";
+
+export const addUser = async (user: IFormProps) => {
+    const res = await fetch('https://codestackform.azurewebsites.net/AddUser', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+
+    console.log('Response: ' + res)
+    if (!res.ok) {
+        const message = 'an error has occured! ' + res.status;
+        throw new Error(message);
+    }
+
+    const data = await res.json()
+    console.log(data)
+    return data;
+    // console.log(data);
+   
 }
