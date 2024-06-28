b8b0a700ab2460bd395ec8a7292e3c7242d1546c
SNOWWWWWW4 <lescober7@gmail.com>
2024-06-28 10:24:16 -0700

    Resetting and re-roganizing the files so we have a cleaner template to work on. Getting ready for the new requirements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import FormField from "@/components/FormField";
-
 
 export default function Home() {
 
   return (
     <main className="min-h-screen grid grid-cols-1 computer:grid-cols-2 justify-between p-4 mobile:p-10 tablet:p-24 ">
-      <section className="flex flex-col justify-center items-center bg-slate-200 rounded-lg mobile:bg-white mobile:mx-10">
-        <h1 className="text-3xl font-bold ">This is a form!</h1>
-        <h4 className="m-4 text-balance text-center">A very strong password is required for security!</h4>
-      </section>
-      <section className="flex flex-col justify-center items-center">
-        <FormField />
-      </section>
+      <h1 className="text-3xl font-bold text-center">This is the login/create/forgot password page</h1>
     </main>
   );
 }
--- src/app/pages/landing/page.tsx
+++ src/app/pages/landing/page.tsx
@@ -1,10 +1,9 @@
-import SuccessPage from '@/components/SuccessPage'
 import React from 'react'
 
 export default function page() {
   return (
     <>
-      <SuccessPage />
+      
     </>
   )
 }
