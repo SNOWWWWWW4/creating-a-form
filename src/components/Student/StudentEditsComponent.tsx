4e1a209cf33cae29919165795a70a4ff8882dbf2
SNOWWWWWW4 <lescober7@gmail.com>
2024-07-01 20:45:05 -0700

    fixed import errors

--- src/components/Student/StudentEditsComponent.tsx
+++ src/components/Student/StudentEditsComponent.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import useAuth from '@/hooks/useAuth';
+import useFormValidation from '@/hooks/useFormValidation';
 import { Check, Visibility, VisibilityOff } from '@mui/icons-material';
 import {
   Alert,
@@ -16,7 +16,9 @@ import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
 import { green, red } from '@mui/material/colors';
 
-const StudentEditsComponent = (props: {setIsEdit:(input:boolean)=>void}) => {
+const StudentEditsComponent = (props: {
+  setIsEdit: (input: boolean) => void;
+}) => {
   const {
     firstName,
     setFirstName,
@@ -36,31 +38,15 @@ const StudentEditsComponent = (props: {setIsEdit:(input:boolean)=>void}) => {
     phone,
     setPhone,
     phoneError,
-    password,
-    setPassword,
-    passwordError,
-    confirmPassword,
-    setConfirmPassword,
-    confirmPasswordError,
     handleSubmit,
-    passwordStrength,
-    loginSuccess,
-    setLoginSuccess,
-  } = useAuth();
+    formSuccessful,
+    setFormSuccessful,
+  } = useFormValidation();
 
   const handleKeydown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') handleSubmit();
   };
 
-  const [visible, setVisible] = useState(false);
-  const [visibleTwo, setVisibleTwo] = useState(false);
-
-  const [cap, setCap] = useState(false);
-  const [spec, setSpec] = useState(false);
-  const [num, setNum] = useState(false);
-
-  const handleEyeClick = () => setVisible(!visible);
-  const handleEyeClickTwo = () => setVisibleTwo(!visibleTwo);
 
   const handleClose = (
     event?: React.SyntheticEvent | Event,
@@ -69,7 +55,7 @@ const StudentEditsComponent = (props: {setIsEdit:(input:boolean)=>void}) => {
     if (reason === 'clickaway') {
       return;
     }
-    setLoginSuccess(false);
+    setFormSuccessful(false);
   };
 
   const today = new Date().toISOString().split('T')[0];
@@ -80,131 +66,121 @@ const StudentEditsComponent = (props: {setIsEdit:(input:boolean)=>void}) => {
 
   return (
     <>
-    <div className='  absolute top-1/2 -translate-y-1/2 z-[100] justify-center  flex w-full bg-black bg-opacity-80 h-screen  items-center'>
+      <div className='  absolute top-1/2 -translate-y-1/2 z-[100] justify-center  flex w-full bg-black bg-opacity-80 h-screen  items-center'>
+        <div className='w-full mx-[100px] bg-[#ffffff] shadow-md rounded-lg py-8 px-10 lg:py-14 lg:px-16 space-y-4'>
+          <FormControl fullWidth>
+            <div className='grid grid-cols-2 gap-4'>
+              <TextField
+                label='First Name'
+                variant='outlined'
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                error={!!firstNameError}
+                helperText={firstNameError}
+                onKeyDown={handleKeydown}
+                className='col-span-2 sm:col-span-1'
+                required
+              />
+              <TextField
+                label='Last Name'
+                variant='outlined'
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                error={!!lastNameError}
+                helperText={lastNameError}
+                onKeyDown={handleKeydown}
+                className='col-span-2 sm:col-span-1'
+                required
+              />
+              <TextField
+                variant='outlined'
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+                error={!!dobError}
+                helperText={dobError}
+                onKeyDown={handleKeydown}
+                className='col-span-2 sm:col-span-1'
+                type='date'
+                InputProps={{
+                  inputProps: { min: formattedHundredYearsAgo, max: today },
+                }}
+                required
+              />
+              <TextField
+                label='Phone Number'
+                variant='outlined'
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                error={!!phoneError}
+                helperText={phoneError}
+                onKeyDown={handleKeydown}
+                className='col-span-2 sm:col-span-1'
+              />
+              <TextField
+                label='Email'
+                variant='outlined'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={!!emailError}
+                helperText={emailError}
+                onKeyDown={handleKeydown}
+                className='col-span-2'
+                required
+              />
+              <TextField
+                label='Address'
+                variant='outlined'
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                error={!!addressError}
+                helperText={addressError}
+                onKeyDown={handleKeydown}
+                className='col-span-2'
+              />
 
-            <div className='w-full mx-[100px] bg-[#ffffff] shadow-md rounded-lg py-8 px-10 lg:py-14 lg:px-16 space-y-4'>
-        <FormControl fullWidth>
-          <div className='grid grid-cols-2 gap-4'>
-            <TextField
-              label='First Name'
-              variant='outlined'
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              error={!!firstNameError}
-              helperText={firstNameError}
-              onKeyDown={handleKeydown}
-              className='col-span-2 sm:col-span-1'
-              required
-            />
-            <TextField
-              label='Last Name'
-              variant='outlined'
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              error={!!lastNameError}
-              helperText={lastNameError}
-              onKeyDown={handleKeydown}
-              className='col-span-2 sm:col-span-1'
-              required
-            />
-            <TextField
-              variant='outlined'
-              value={dob}
-              onChange={(e) => setDob(e.target.value)}
-              error={!!dobError}
-              helperText={dobError}
-              onKeyDown={handleKeydown}
-              className='col-span-2 sm:col-span-1'
-              type='date'
-              InputProps={{
-                inputProps: { min: formattedHundredYearsAgo, max: today },
-              }}
-              required
-            />
-            <TextField
-              label='Phone Number'
-              variant='outlined'
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              error={!!phoneError}
-              helperText={phoneError}
-              onKeyDown={handleKeydown}
-              className='col-span-2 sm:col-span-1'
-            />
-            <TextField
-              label='Email'
-              variant='outlined'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              error={!!emailError}
-              helperText={emailError}
-              onKeyDown={handleKeydown}
-              className='col-span-2'
-              required
-            />
-            <TextField
-              label='Address'
-              variant='outlined'
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              error={!!addressError}
-              helperText={addressError}
-              onKeyDown={handleKeydown}
-              className='col-span-2'
-            />
-
-            <div className='flex'>
+              <div className='flex'>
                 <Button
-              onClick={() => {
+                  onClick={() => {
+                    props.setIsEdit(false);
+                  }}
+                  variant='contained'
+                  size='large'
+                  className='col-span-2 justify-center'
+                >
+                  Cancel
+                </Button>
+                <Button
+                  onClick={() => {
+                    handleSubmit();
 
                     props.setIsEdit(false);
-
-
-              }}
-              variant='contained'
-              size='large'
-              className='col-span-2 justify-center'
-            >
-              Cancel
-            </Button>
-            <Button
-              onClick={() => {
-                handleSubmit()
-
-                    props.setIsEdit(false);
-
-
-              }}
-              variant='contained'
-              size='large'
-              className='col-span-2 justify-center'
-            >
-              Submit
-            </Button>
+                  }}
+                  variant='contained'
+                  size='large'
+                  className='col-span-2 justify-center'
+                >
+                  Submit
+                </Button>
+              </div>
             </div>
-
-            
-          </div>
-        </FormControl>
-      </div>
-      <Snackbar
-        open={loginSuccess}
-        autoHideDuration={3500}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      >
-        <Alert
+          </FormControl>
+        </div>
+        <Snackbar
+          open={formSuccessful}
+          autoHideDuration={3500}
           onClose={handleClose}
-          severity='success'
-          variant='filled'
-          sx={{ width: '100%' }}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
         >
-          Successfully Submitted. Loading...
-        </Alert>
-      </Snackbar>
-
-</div>
-      
+          <Alert
+            onClose={handleClose}
+            severity='success'
+            variant='filled'
+            sx={{ width: '100%' }}
+          >
+            Successfully Submitted. Loading...
+          </Alert>
+        </Snackbar>
+      </div>
     </>
   );
 };
