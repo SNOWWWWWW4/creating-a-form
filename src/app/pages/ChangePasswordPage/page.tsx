15b39297f9b5cbb26b2e933d8fcf87e815dc8054
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2024-07-03 11:54:08 -0700

    fixed order of image and form

--- src/app/pages/ChangePasswordPage/page.tsx
+++ src/app/pages/ChangePasswordPage/page.tsx
@@ -5,14 +5,17 @@ import ChangePassForm from '@/components/form/ChangePassForm';
 const ChangePasswordPage = () => {
 
   return (
-    <div className='min-h-screen bg-mainBg bg-cover grid grid-cols-1 computer:grid-cols-2'>
+    <div className='min-h-screen   bg-mainBg bg-cover grid grid-cols-1 computer:grid-cols-2'>
+     <div className=' order-2  md:mt-0 grid items-center'>
+     <ChangePassForm />
+     </div>
       
-      <ChangePassForm />
-
-      <div className='flex flex-col justify-center items-center'>
+      
+      <div className='flex order-1  flex-col justify-center items-center'>
         <img className='w-96' src='/moon3.png'/>
       </div>
     </div>
+
   );
 }
 
